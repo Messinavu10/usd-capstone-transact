b195cf0778f461a65a9b9507bcbac4c8f65d2ed1
Paul Lu <sininglu@sandiego.edu>
2023-04-03 15:12:52 -0700

    Co-authored-by: Mrinal Virnave <mrinalvirnave@users.noreply.github.com>

--- src/services.js
+++ src/services.js
@@ -78,75 +78,75 @@ async function connectAndQuery(email) {
   return rows;
 }
 
-var mainApp = require('./app.js');
+// var mainApp = require('./app.js');
 
-console.log(mainApp);
+// console.log(mainApp);
 /**
  * This method is called from the UI to initiate the presentation of the verifiable credential
- */
-mainApp.app.get("/api/verifier/presentation-request", async (req, res) => {
-  requestTrace(req);
-  var id = req.session.id;
-  // prep a session state of 0
-  mainApp.sessionStore.get(id, (error, session) => {
-    var sessionData = {
-      status: 0,
-      message: "Waiting for QR code to be scanned",
-    };
-    if (session) {
-      session.sessionData = sessionData;
-      mainApp.sessionStore.set(id, session);
-    }
-  });
-  // get the Access Token
-  var accessToken = "";
-  try {
-    const result = await mainApp.msalCca.acquireTokenByClientCredential(
-      mainApp.msalClientCredentialRequest
-    );
-    if (result) {
-      accessToken = result.accessToken;
-    }
-  } catch {
-    console.log("failed to get access token");
-    res.status(401).json({
-      error: "Could not acquire credentials to access your Azure Key Vault",
-    });
-    return;
-  }
-  console.log(`accessToken: ${accessToken}`);
-  // modify the callback method to make it easier to debug
-  // with tools like ngrok since the URI changes all the time
-  // this way you don't need to modify the callback URL in the payload every time
-  // ngrok changes the URI
-  presentationConfig.callback.url = `https://${req.hostname}/api/verifier/presentation-request-callback`;
-  presentationConfig.callback.state = id;
+//  */
+// mainApp.app.get("/api/verifier/presentation-request", async (req, res) => {
+//   requestTrace(req);
+//   var id = req.session.id;
+//   // prep a session state of 0
+//   mainApp.sessionStore.get(id, (error, session) => {
+//     var sessionData = {
+//       status: 0,
+//       message: "Waiting for QR code to be scanned",
+//     };
+//     if (session) {
+//       session.sessionData = sessionData;
+//       mainApp.sessionStore.set(id, session);
+//     }
+//   });
+//   // get the Access Token
+//   var accessToken = "";
+//   try {
+//     const result = await mainApp.msalCca.acquireTokenByClientCredential(
+//       mainApp.msalClientCredentialRequest
+//     );
+//     if (result) {
+//       accessToken = result.accessToken;
+//     }
+//   } catch {
+//     console.log("failed to get access token");
+//     res.status(401).json({
+//       error: "Could not acquire credentials to access your Azure Key Vault",
+//     });
+//     return;
+//   }
+//   console.log(`accessToken: ${accessToken}`);
+//   // modify the callback method to make it easier to debug
+//   // with tools like ngrok since the URI changes all the time
+//   // this way you don't need to modify the callback URL in the payload every time
+//   // ngrok changes the URI
+//   presentationConfig.callback.url = `https://${req.hostname}/api/verifier/presentation-request-callback`;
+//   presentationConfig.callback.state = id;
 
-  console.log("VC Client API Request");
-  var client_api_request_endpoint = `${mainApp.config.msIdentityHostName}verifiableCredentials/createPresentationRequest`;
-  console.log(client_api_request_endpoint);
-  var payload = JSON.stringify(presentationConfig);
-  console.log(payload);
-  const fetchOptions = {
-    method: "POST",
-    body: payload,
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": payload.length.toString(),
-      Authorization: `Bearer ${accessToken}`,
-    },
-  };
+//   console.log("VC Client API Request");
+//   var client_api_request_endpoint = `${mainApp.config.msIdentityHostName}verifiableCredentials/createPresentationRequest`;
+//   console.log(client_api_request_endpoint);
+//   var payload = JSON.stringify(presentationConfig);
+//   console.log(payload);
+//   const fetchOptions = {
+//     method: "POST",
+//     body: payload,
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Content-Length": payload.length.toString(),
+//       Authorization: `Bearer ${accessToken}`,
+//     },
+//   };
 
-  const response = await fetch(client_api_request_endpoint, fetchOptions);
-  var resp = await response.json();
+//   const response = await fetch(client_api_request_endpoint, fetchOptions);
+//   var resp = await response.json();
 
-  // the response from the VC Request API call is returned to the caller (the UI). It contains the URI to the request which Authenticator can download after
-  // it has scanned the QR code. If the payload requested the VC Request service to create the QR code that is returned as well
-  // the javascript in the UI will use that QR code to display it on the screen to the user.
-  resp.id = id; // add id so browser can pull status
-  console.log("VC Client API Response");
-  console.log(resp);
-  res.status(200).json(resp);
-});
+//   // the response from the VC Request API call is returned to the caller (the UI). It contains the URI to the request which Authenticator can download after
+//   // it has scanned the QR code. If the payload requested the VC Request service to create the QR code that is returned as well
+//   // the javascript in the UI will use that QR code to display it on the screen to the user.
+//   resp.id = id; // add id so browser can pull status
+//   console.log("VC Client API Response");
+//   console.log(resp);
+//   res.status(200).json(resp);
+// });
 
 module.exports = getRole;
