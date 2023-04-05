26015e2726e7a07ba854a097538739c179aa2a04
Mrinal Virnave <3670383+mrinalvirnave@users.noreply.github.com>
2023-04-05 10:52:51 -0700

    workin setup

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,5 @@
 /** @format */
-const axios = require('axios');
 const sql = require('mssql');
-const qs = require('qs');
-const jmespath = require('jmespath');
-const verifiedid = require('./services/verified_id');
 const getAttributes = require("./getattributes");
 
 // create another service file.
