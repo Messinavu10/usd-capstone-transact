5c08afb497e5e86701a01ca46fb3344eeada0ded
Mrinal Virnave <3670383+mrinalvirnave@users.noreply.github.com>
2023-04-20 17:26:51 -0700

    removed extra call to listCredType

--- src/controller.js
+++ src/controller.js
@@ -246,7 +246,6 @@ exports.getVerifierPageQR = async (req, res, next) => {
   // an array of objects
   //[{ "role": "issuer"},{ "role": "verifier"},] make it an array of strings condensation
   if (req.session?.idTokenClaims?.emails[0]) {
-    credentialTypes = await verifiedid.listCredType();
     queryRoles = await getRole(req.session.idTokenClaims.emails[0]); // call the functions
     //console.log(queryRoles["recordset"][0]["roleName"]); // {roleName: 'Holder'}
   }
